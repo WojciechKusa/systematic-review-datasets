0000b4d1790ffb3aa41294d1d2c570c8096577b6
Wojciech Kusa <WojciechKusa@users.noreply.github.com>
2023-05-11 11:49:10 +0200

    feat(ec2s): add project stubs (#4)
    * feat(ec2s): add project stubs
    
    * fix: minor fixes based on code review

---
+++ ec2s/ec2s/cochrane_web_parser.py
@@ -0,0 +1,256 @@
+import contextlib
+import logging
+import re
+from typing import Optional, Union
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def get_safe_text(element) -> str:
+    return element.text if element else ""
+
+
+def _find_doi(urls: list[str]) -> Optional[str]:
+    """searches for DOI in urls"""
+    doi = None
+    for url in urls:
+        if x := re.search(r"https://doi.org/(.*)", url):
+            doi = f"https://doi.org/{str(x[1])}"
+    return doi
+
+
+def _find_pubmed_id(citation_text: str, urls: list[str]) -> Optional[str]:
+    """Find PubMed ID in citation text. Example: [PUBMED: 123456].
+    Alternatively there might be a link."""
+    _pubmed_id = (
+        str(x[1]) if (x := re.search(r"\[PUBMED: (\d+)\]", citation_text)) else None
+    )
+    if not _pubmed_id:
+        for url in urls:
+            if x := re.search(r"https://www.ncbi.nlm.nih.gov/pubmed/(\d+)", url):
+                _pubmed_id = str(x[1])
+    return _pubmed_id
+
+
+def get_citation_data(citation) -> dict[str, Union[Optional[str], list[str]]]:
+    _reference_id = citation.attrs.get("id")
+
+    citation_class = [x.attrs.get("class") for x in citation.find_all()][0]
+    citation_text = citation.text
+
+    title = get_safe_text(citation.find("span", {"class": "citation-title"}))
+    journal = get_safe_text(citation.find("span", {"class": "citation"}))
+    year = get_safe_text(citation.find("span", {"class": "pubYear"}))
+    volume = get_safe_text(citation.find("span", {"class": "volume"}))
+
+    citation_div = citation.find("div", class_=citation_class)
+    first_child = citation_div.find_all("span")[0]
+    last_child = citation_div.find_all("span")[-1]
+
+    authors = citation_div.text[: citation_div.text.index(first_child.text)]
+    # fixme get text after last child
+    citation_footer = citation_div.text[
+        citation_div.text.rindex(last_child.text) + len(last_child.text) :
+    ]
+
+    urls = [
+        citation_link.attrs.get("href")
+        for citation_link in citation.find_all("a", {"class": "citation-link"})
+    ]
+    pubmed_id = _find_pubmed_id(citation_text, urls=urls)
+    doi = _find_doi(urls)
+    return {
+        "reference_id": _reference_id,
+        "authors": authors,
+        "title": title,
+        "journal": journal,
+        "year": year,
+        "volume": volume,
+        "citation_footer": citation_footer,
+        "citation_type": citation_class,
+        "citation": citation_text,
+        "urls": urls,
+        "pubmed_id": pubmed_id,
+        "doi": doi,
+    }
+
+
+def parse_cochrane_review_reference_section(
+    references_section, reference_class: str, reference_category: str
+) -> pd.DataFrame:
+    _studies = []
+    for _study in references_section.find_all(
+        "div", {"class": f"bibliographies {reference_class}"}
+    ):
+        header = _study.find("div", {"class": "reference-title-banner"}).text
+        _study_id = _study.attrs.get("id")
+        for citation in _study.find_all("div", {"class": "bibliography-section"}):
+            citation_data = get_citation_data(citation)
+            citation_data["header"] = header
+            citation_data["reference_type"] = reference_category
+            citation_data["study_id"] = _study_id
+
+            _studies.append(citation_data)
+
+    return pd.DataFrame(_studies)
+
+
+def get_exclusion_reasons(exclusion_section, df: pd.DataFrame) -> pd.DataFrame:
+    df["exclusion_reason"] = ""
+    logging.debug(
+        f"Found {len(list(exclusion_section.find_all('tr')))} documents with exclusion reasons"
+    )
+    for _excluded_paper in exclusion_section.find("tbody").find_all("tr"):
+        study_id = (
+            list(_excluded_paper.find_all("td"))[0]
+            .find("a")
+            .attrs.get("href")
+            .split("#")[-1]
+        )
+        exclusion_reason = list(_excluded_paper.find_all("td"))[1].text
+
+        df.loc[df["study_id"] == study_id, "exclusion_reason"] = exclusion_reason
+
+    return df
+
+
+def get_reference_page(url: str, headers: dict[str, str]) -> Optional[BeautifulSoup]:
+    r = requests.get(url, headers=headers)
+    if r.status_code != 200:
+        logger.error(f"Error {r.status_code} for {url}")
+        return None
+
+    return BeautifulSoup(r.content, "html.parser")
+
+
+def safe_convert_to_float(number):
+    try:
+        return float(number)
+    except ValueError:
+        number = number.replace("‐", "-").strip("'")
+        return float(number)
+
+
+def get_effect_size(
+    text: str,
+) -> tuple[Optional[float], Optional[float], Optional[float]]:
+    if not text:
+        return None, None, None
+    if len(text.split("[")) == 1:
+        return None, None, None
+    effect_size = text.split("[")[0].strip()
+    lower_ci = text.split("[")[1].split(",")[0].strip()
+    upper_ci = text.split("[")[1].split(",")[1].split("]")[0].strip()
+    return (
+        safe_convert_to_float(effect_size),
+        safe_convert_to_float(lower_ci),
+        safe_convert_to_float(upper_ci),
+    )
+
+
+def postprocess_comparison_table(df):
+    current_outcome_name = ""
+    for index, row in df.iterrows():
+        _id = row["Outcome or subgroup title"].split(" ")[0]
+        if "." in _id:
+            outcome_id = _id.split(".")[0]
+            subgroup_id = _id.split(".")[1]
+        else:
+            outcome_id = _id
+            subgroup_id = None
+        df.loc[index, "outcome_id"] = outcome_id
+        df.loc[index, "subgroup_id"] = subgroup_id
+
+        title = " ".join(row["Outcome or subgroup title"].split(" ")[1:])
+        if not subgroup_id:
+            current_outcome_name = title[:-16].strip()
+            df.loc[index, "outcome_name"] = current_outcome_name
+            df.loc[index, "subgroup_name"] = None
+        else:
+            df.loc[index, "outcome_name"] = current_outcome_name
+            df.loc[index, "subgroup_name"] = title
+
+    return df
+
+
+def parse_data_and_analyses_section(url: str, headers: dict[str, str]) -> pd.DataFrame:
+    soup = get_reference_page(url, headers)
+    if not soup:
+        return pd.DataFrame()
+
+    data_section = soup.find("section", {"id": "dataAndAnalyses"})
+
+    out_df = pd.DataFrame()
+    for comparison_id, comparison in enumerate(
+        data_section.find_all("div", {"class": "table"})
+    ):
+        comparison_name = (
+            comparison.find("div", {"class": "table-heading"})
+            .find("span", {"class": "table-title"})
+            .text.strip()
+        )
+        try:
+            df = pd.read_html(
+                str(comparison.find("table", {"class": "comparison"})),
+                flavor="html5lib",
+            )[0]
+
+            if df.columns.nlevels > 1:
+                df.columns = df.columns.droplevel(list(range(1, df.columns.nlevels)))
+        except ValueError:
+            logger.error(f"Error reading table {comparison_id + 1} for {url}")
+            continue
+
+        df = df.dropna(axis=0, how="all").reset_index(drop=True)
+        df["comparison_name"] = comparison_name
+        df["comparison_id"] = comparison_id + 1
+
+        with contextlib.suppress(AttributeError):
+            effects = df["Effect size"].apply(get_effect_size).apply(pd.Series)
+            df["effect_size"] = effects[0]
+            df["lower_ci"] = effects[1]
+            df["upper_ci"] = effects[2]
+
+        df = df[~df[df.columns[0]].str.startswith("Open in figure viewer")]
+        df = postprocess_comparison_table(df)
+        out_df = pd.concat([out_df, df])
+
+    return out_df.reset_index(drop=True)
+
+
+def parse_cochrane_references(url: str, headers: dict[str, str]) -> pd.DataFrame:
+    soup = get_reference_page(url, headers)
+    if not soup:
+        return pd.DataFrame()
+
+    references_section = soup.find("section", {"class": "references bibliographies"})
+
+    reference_types = {
+        "included": "references_includedStudies",
+        "excluded": "references_excludedStudies",
+        "additional": "references_additionalReferences",
+        "awaiting_assessment": "references_awaitingAssessmentStudies",
+        "other_versions": "references_otherVersions",
+    }
+    out_df = pd.DataFrame()
+    for reference_category, reference_class in reference_types.items():
+        df = parse_cochrane_review_reference_section(
+            references_section=references_section,
+            reference_class=reference_class,
+            reference_category=reference_category,
+        )
+
+        if reference_category == "excluded":
+            df = get_exclusion_reasons(
+                soup.find("section", {"class": "characteristicsOfExcludedStudies"}),
+                df,
+            )
+
+        out_df = pd.concat([out_df, df])
+
+    return out_df.reset_index(drop=True)
---
+++ ec2s/ec2s/match_references.py
@@ -0,0 +1,169 @@
+import re
+
+import pandas as pd
+import requests
+from Bio import Entrez, Medline
+from tqdm import tqdm
+from urllib.error import HTTPError
+from http.client import IncompleteRead
+
+
+S2_API_KEY_FILE = "data/raw/semanticscholar_api_key.txt"
+API_ENDPOINT = "https://api.semanticscholar.org/graph/v1/paper/"
+
+try:
+    with open(S2_API_KEY_FILE, "r", encoding="utf-8") as f:
+        api_key = f.read().strip()
+except FileNotFoundError:
+    api_key = ""
+
+
+def get_paper_id_by_doi(doi):
+    url = f"{API_ENDPOINT}DOI:{doi}?fields=url,title,externalIds,citationCount"
+    r = requests.get(url, headers={"x-api-key": api_key})
+    return r.json() if r.status_code == 200 else None
+
+
+def search_pubmed(query: str) -> str:
+    """Search pubmed for a query and return the pubmed id."""
+    handle = Entrez.esearch(db="pubmed", term=query)
+    record = Entrez.read(handle)
+    return str(record["IdList"][0]) if len(record["IdList"]) > 0 else ""
+
+
+def assign_pubmed_ids(included_df: pd.DataFrame) -> pd.DataFrame:
+    """Search for data in pubmed based on the study title and authors."""
+    Entrez.email = "test@gmail.com"
+    included_df["PUBMED_ID_TITLE_AUTHORS"] = ""
+    included_df["PUBMED_ID_TITLE"] = ""
+    for index, row in tqdm(included_df.iterrows(), total=len(included_df)):
+        title = row["title"]
+        authors = row["authors"]
+
+        _pubmed_id = search_pubmed(f"{title} AND {authors}")
+        included_df.loc[index, "PUBMED_ID_TITLE_AUTHORS"] = _pubmed_id
+
+        _pubmed_id = search_pubmed(f"{title}")
+        if _pubmed_id:
+            try:
+                handle = Entrez.efetch(
+                    db="pubmed", id=_pubmed_id, rettype="medline", retmode="text"
+                )
+                if record := list(Medline.parse(handle))[0]:
+                    _title = str(record.get("TI", "").strip(' .,;:!?-"'))
+                    _title = re.sub(r"[^a-zA-Z0-9]+", " ", _title).lower().strip()
+                    if (
+                        _title
+                        != re.sub(r"[^a-zA-Z0-9]+", " ", str(title)).lower().strip()
+                    ):
+                        _pubmed_id = ""
+            except (ValueError, HTTPError, IncompleteRead):
+                _pubmed_id = ""
+        included_df.loc[index, "PUBMED_ID_TITLE"] = _pubmed_id
+
+        s2 = get_paper_id_by_doi(
+            str(row.get("doi", "")).replace("https://doi.org/", "")
+        )
+        included_df.loc[index, "PUBMED_ID_S2"] = (
+            s2["externalIds"].get("PubMed", "") if s2 else ""
+        )
+        included_df.loc[index, "N_CITATIONS"] = s2["citationCount"] if s2 else ""
+
+    included_df = normalise_pubmed_ids(df=included_df)
+    return included_df
+
+
+def normalise_pubmed_ids(df: pd.DataFrame) -> pd.DataFrame:
+    """There are 4 different columns with pubmed ids. We normalise them to one column.
+    Priority: pubmed_id, PUBMED_ID_S2, PUBMED_ID_TITLE_AUTHORS, PUBMED_ID_TITLE
+    cochrane web > doi from s2 > title + authors from pubmed > title from pubmed"""
+    columns = [
+        "pubmed_id",
+        "PUBMED_ID_S2",
+        "PUBMED_ID_TITLE_AUTHORS",
+        "PUBMED_ID_TITLE",
+    ]
+    for column in columns:
+        df[column] = df[column].fillna("")
+        df[column] = df[column].astype(str)
+        df[column] = df[column].replace("", None)
+
+    df["PUBMED_ID"] = df["pubmed_id"]
+    df["PUBMED_ID"] = df["PUBMED_ID"].fillna(df["PUBMED_ID_S2"])
+    df["PUBMED_ID"] = df["PUBMED_ID"].fillna(df["PUBMED_ID_TITLE_AUTHORS"])
+    df["PUBMED_ID"] = df["PUBMED_ID"].fillna(df["PUBMED_ID_TITLE"])
+
+    df["PUBMED_ID"] = df["PUBMED_ID"].fillna("")
+    df["PUBMED_ID"] = df["PUBMED_ID"].astype(str)
+    return df
+
+
+def merge_papers_to_studies(
+    web_references_df: pd.DataFrame,
+    revman_df: pd.DataFrame,
+    review_id: str,
+    output_path: str,
+) -> pd.DataFrame:
+    """Merge the papers to the studies."""
+
+    web_references_df = web_references_df[
+        web_references_df["reference_type"] == "included"
+    ].reset_index()
+
+    merged_df = match_web_to_revman_studies(web_references_df, revman_df)
+    not_found_df = web_references_df[
+        ~web_references_df["header"].isin(merged_df["header"])
+    ]
+    not_found_other = revman_df[~revman_df["STUDY_NAME"].isin(merged_df["STUDY_NAME"])]
+
+    if not not_found_df.empty or not not_found_other.empty:
+        print(review_id)
+        print(not_found_df)
+        print(not_found_other)
+
+    print(
+        f"{review_id}\t {len(web_references_df)=}, {len(revman_df)=}, {len(merged_df)=}, {len(not_found_df)=}, {len(not_found_other)=}"
+    )
+    if not not_found_df.empty:
+        not_found_df.to_csv(
+            f"{output_path}/{review_id}/not_found.csv",
+            index=False,
+        )
+
+    return assign_pubmed_ids(merged_df)
+
+
+def match_web_to_revman_studies(
+    web_references_df: pd.DataFrame, revman_df: pd.DataFrame
+) -> pd.DataFrame:
+    """In web_references_df we match column 'header' to the column STUDY_NAME in revman_df.
+    First the web_references_df is preprocessed to strip of additional characters."""
+    web_references_column = "header"
+    revman_column = "STUDY_NAME"
+
+    web_references_df[web_references_column] = web_references_df[
+        web_references_column
+    ].str.replace(r"\[.*\]", "", regex=True)
+    web_references_df[web_references_column] = web_references_df[
+        web_references_column
+    ].str.replace(r"\{.*\}", "", regex=True)
+    web_references_df = normalise_column(web_references_df, web_references_column)
+
+    revman_df = normalise_column(revman_df, revman_column)
+    return pd.merge(
+        web_references_df,
+        revman_df,
+        left_on=web_references_column,
+        right_on=revman_column,
+        how="inner",
+    )
+
+
+def normalise_column(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    """Normalise the column by removing brackets and extra spaces.
+    Also replace the unicode character '‐' with a hyphen."""
+    df[column] = df[column].str.replace(r"\(.*\)", "", regex=True)
+    df[column] = df[column].str.replace(r"\s+", " ", regex=True)
+    df[column] = df[column].str.replace("‐", "-")
+    df[column] = df[column].str.strip()
+    return df
