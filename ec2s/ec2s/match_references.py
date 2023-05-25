93fd3eec78bde73992bd6d9a3a4f23804efbac82
Wojciech Kusa <WojciechKusa@users.noreply.github.com>
2023-05-25 16:56:49 +0200

    feat(data): add PDF links from CORE and S2 to references (#7)

--- ec2s/ec2s/match_references.py
+++ ec2s/ec2s/match_references.py
@@ -8,24 +8,43 @@ from urllib.error import HTTPError
 from http.client import IncompleteRead
 
 
-S2_API_KEY_FILE = "data/raw/semanticscholar_api_key.txt"
-API_ENDPOINT = "https://api.semanticscholar.org/graph/v1/paper/"
+S2_API_KEY_FILE = "../data/raw/semanticscholar_api_key.txt"
+CORE_API_KEY_FILE = "../data/raw/core_api_key.txt"
+S2_API_ENDPOINT = "https://api.semanticscholar.org/graph/v1/paper/"
+CORE_API_ENDPOINT = "https://api.core.ac.uk/v3/discover"
 
 try:
     with open(S2_API_KEY_FILE, "r", encoding="utf-8") as f:
-        api_key = f.read().strip()
+        s2_api_key = f.read().strip()
 except FileNotFoundError:
-    api_key = ""
+    s2_api_key = ""
+
+try:
+    with open(CORE_API_KEY_FILE, "r", encoding="utf-8") as f:
+        core_api_key = f.read().strip()
+except FileNotFoundError:
+    core_api_key = ""
 
 
 def get_paper_id_by_doi(doi):
-    url = f"{API_ENDPOINT}DOI:{doi}?fields=url,title,externalIds,citationCount"
-    r = requests.get(url, headers={"x-api-key": api_key})
+    url = f"{S2_API_ENDPOINT}DOI:{doi}?fields=url,title,externalIds,citationCount,openAccessPdf"
+    r = requests.get(url, headers={"x-api-key": s2_api_key})
+    return r.json() if r.status_code == 200 else None
+
+
+def get_paper_full_text_by_doi_core(doi):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + core_api_key,
+    }
+    r = requests.post(url=CORE_API_ENDPOINT, headers=headers, json={"doi": doi})
     return r.json() if r.status_code == 200 else None
 
 
 def search_pubmed(query: str) -> str:
     """Search pubmed for a query and return the pubmed id."""
+    if not query:
+        return ""
     handle = Entrez.esearch(db="pubmed", term=query)
     record = Entrez.read(handle)
     return str(record["IdList"][0]) if len(record["IdList"]) > 0 else ""
@@ -68,6 +87,16 @@ def assign_pubmed_ids(included_df: pd.DataFrame) -> pd.DataFrame:
             s2["externalIds"].get("PubMed", "") if s2 else ""
         )
         included_df.loc[index, "N_CITATIONS"] = s2["citationCount"] if s2 else ""
+        included_df.loc[index, "S2_PDF_LINK"] = (
+            s2["openAccessPdf"]["url"] if s2 and s2["openAccessPdf"] else ""
+        )
+
+        core = get_paper_full_text_by_doi_core(
+            str(row.get("doi", "")).replace("https://doi.org/", "")
+        )
+        included_df.loc[index, "CORE_PDF_LINK"] = (
+            core["fullTextLink"] if core and core["fullTextLink"] else ""
+        )
 
     included_df = normalise_pubmed_ids(df=included_df)
     return included_df
@@ -96,6 +125,11 @@ def normalise_pubmed_ids(df: pd.DataFrame) -> pd.DataFrame:
     df["PUBMED_ID"] = df["PUBMED_ID"].fillna("")
     df["PUBMED_ID"] = df["PUBMED_ID"].astype(str)
     return df
+
+
+def expand_references_details(web_references_df: pd.DataFrame) -> pd.DataFrame:
+    web_references_df = assign_pubmed_ids(web_references_df)
+    return web_references_df
 
 
 def merge_papers_to_studies(
