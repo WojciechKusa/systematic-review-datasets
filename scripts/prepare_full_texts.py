51ef6c19feba4fc1fba8a3500f5395192f77e1b9
Wojciech Kusa <wojciech.kusa@gmail.com>
2023-05-26 19:55:53 +0200

    fix(full-text): improve full text parsing script

--- scripts/prepare_full_texts.py
+++ scripts/prepare_full_texts.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import ssl
 
 import pandas as pd
 import requests
@@ -12,11 +13,15 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def download_file(url, download_path):
+def download_file(url: str, download_path: str):
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0",
     }
-    r = requests.get(url, stream=True, allow_redirects=True, headers=headers)
+    try:
+        r = requests.get(url, stream=True, allow_redirects=True, headers=headers, verify=ssl.CERT_NONE)
+    except (requests.exceptions.InvalidSchema, requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as e:
+        logger.warning(f"Invalid schema for {url}, error: {e}")
+        return
     with open(download_path, "wb") as f:
         for ch in r:
             f.write(ch)
@@ -35,12 +40,10 @@ if __name__ == "__main__":
             if os.path.isdir(f"{dataset}/{review}")
         ]
         for review in tqdm(reviews):
-            print(review)
             df = pd.read_csv(f"{dataset}/{review}/references.csv")
             save_folder = f"{dataset}/{review}/pdfs"
 
-            logger.info("Downloading PDFs")
-            for index, row in df.iterrows():
+            for index, row in tqdm(df.iterrows(), total=len(df)):
                 if "CORE_PDF_LINK" in row:
                     pdf_url = row["CORE_PDF_LINK"]
                 elif "S2_PDF_LINK" in row:
@@ -55,5 +58,4 @@ if __name__ == "__main__":
                     filename = row["reference_id"]
                     download_file(pdf_url, f"{save_folder}/{filename}.pdf")
 
-            logger.info("Processing PDFs")
             client.process("processFulltextDocument", save_folder, n=20, verbose=True)
