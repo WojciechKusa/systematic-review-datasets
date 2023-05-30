659e988d3d67d0f7c6b0842166d626d8f3ba2973
Wojciech Kusa <wojciech.kusa@gmail.com>
2023-05-30 19:11:05 +0200

    fix(data): improve script for converting SIGIR 2017 dataset

--- scripts/convert_sigir2017_dataset.py
+++ scripts/convert_sigir2017_dataset.py
@@ -10,7 +10,7 @@ from ec2s.ec2s.prepare_dataset import prepare_dataset
 
 review_files = "https://raw.githubusercontent.com/ielab/SIGIR2017-SysRev-Collection/master/systematic_reviews.json"
 
-pattern = r'CD(\d+)'
+cochrane_id_pattern = r'CD(\d+)'
 
 
 if __name__ == "__main__":
@@ -30,16 +30,16 @@ if __name__ == "__main__":
     index_id = 1
 
     for review in sigir2017_reviews:
-        match = re.search(pattern, review["url"])
+        match = re.search(cochrane_id_pattern, review["url"])
         if match:
-            review_id = match.group(0)
+            cochrane_id = match.group(0)
         else:
             raise ValueError("Review ID not found")
 
         sigir2017_dataset = prepare_dataset(
-            review_id=review_id, output_data_path=OUTPUT_DATA_PATH
+            review_id=cochrane_id, output_data_path=OUTPUT_DATA_PATH
         )
-        sigir2017_dataset["cochrane_id"] = index_id
+        sigir2017_dataset["cochrane_id"] = cochrane_id
         sigir2017_dataset["dataset_id"] = review["id"]
         sigir2017_dataset["used_in"] = ["SIGIR2017"]
         sigir2017_dataset["review_type"] = review_type
