f84005f16cd6e12a4a94507e0f2424866f6f269d
Wojciech Kusa <wojciech.kusa@gmail.com>
2024-03-08 15:04:55 +0000

    fix: update paths in visualisations

--- visualisation/pages/05_✔️_Dataset_cards.py
+++ visualisation/pages/05_✔️_Dataset_cards.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
         data_config = data_configs[data_idx]
         dataset = load_dataset(
             # FIXME (Wojtek): hardcoded path
-            f"csmed/datasets/datasets/{data_name}/{data_name}.py",
+            f"csmed/datasets/{data_name}/{data_name}.py",
             name=data_config_name,
         )
         # general token length
